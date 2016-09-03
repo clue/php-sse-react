a803854d9497b8eaed4ebeec58f37384f4b5e058
Sascha Nowak <sascha.nowak@netlogix.de>
2016-09-03 17:48:40 +0200

    Add chat example
    Originally by @nlx-sascha, squashed by @clue

---
+++ examples/11-chat.php
@@ -0,0 +1,57 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Clue\React\Sse\BufferedChannel;
+use React\Http\Request;
+use React\Http\Response;
+
+$loop = React\EventLoop\Factory::create();
+$socket = new React\Socket\Server($loop);
+
+$channel = new BufferedChannel();
+
+$http = new React\Http\Server($socket);
+$http->on('request', function (Request $request, Response $response) use ($channel) {
+
+    switch ($request->getPath()) {
+        case '/':
+            $response->writeHead('200', array('Content-Type' => 'text/html'));
+            $response->end(file_get_contents(__DIR__ . '/10-eventsource.html'));
+            $message = array('message' => 'New person connected from '. $request->remoteAddress, 'username' => '');
+            $channel->writeMessage(json_encode($message));
+            return;
+        case '/styles.css':
+            $response->writeHead('200', array('Content-Type' => 'text/css'));
+            $response->end(file_get_contents(__DIR__ . '/10-styles.css'));
+            return;
+        case '/message':
+            $query = $request->getQuery();
+            if (array_key_exists('message', $query)) {
+                $message = array('message' => htmlspecialchars($query['message']), 'username' => htmlspecialchars($query['username']));
+                $channel->writeMessage(json_encode($message));
+            }
+            $response->writeHead('201', array('Content-Type' => 'text/json'));
+            $response->end();
+            return;
+    }
+
+    echo $request->remoteAddress . ' connected' . PHP_EOL;
+
+    $headers = $request->getHeaders();
+    $id = isset($headers['Last-Event-ID']) ? $headers['Last-Event-ID'] : null;
+
+    $response->writeHead(200, array('Content-Type' => 'text/event-stream'));
+    $channel->connect($response, $id);
+
+    $response->on('close', function () use ($response, $channel, $request) {
+        echo $request->remoteAddress . 'disconnected' . PHP_EOL;
+        $channel->disconnect($response);
+    });
+});
+
+$socket->listen(isset($argv[1]) ? $argv[1] : 0, '0.0.0.0');
+
+echo 'Server now listening on http://localhost:' . $socket->getPort() . ' (port is first parameter)' . PHP_EOL;
+
+$loop->run();
