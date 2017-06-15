020f73f480ab6202f91f6722c4dc72029b9b7575
Christian Lück <christian@lueck.tv>
2017-06-15 18:22:51 +0200

    Make chat example more robust against XSS etc.

--- examples/11-chat.php
+++ examples/11-chat.php
@@ -13,13 +13,11 @@ $channel = new BufferedChannel();
 
 $http = new React\Http\Server($socket);
 $http->on('request', function (Request $request, Response $response) use ($channel) {
-
     switch ($request->getPath()) {
         case '/':
             $response->writeHead('200', array('Content-Type' => 'text/html'));
             $response->end(file_get_contents(__DIR__ . '/10-eventsource.html'));
-            $message = array('message' => 'New person connected from '. $request->remoteAddress, 'username' => '');
-            $channel->writeMessage(json_encode($message));
+
             return;
         case '/styles.css':
             $response->writeHead('200', array('Content-Type' => 'text/css'));
@@ -27,27 +25,33 @@ $http->on('request', function (Request $request, Response $response) use ($chann
             return;
         case '/message':
             $query = $request->getQuery();
-            if (array_key_exists('message', $query)) {
-                $message = array('message' => htmlspecialchars($query['message']), 'username' => htmlspecialchars($query['username']));
+            if (isset($query['username'], $query['message'])) {
+                $message = array('message' => $query['message'], 'username' =>$query['username']);
                 $channel->writeMessage(json_encode($message));
             }
             $response->writeHead('201', array('Content-Type' => 'text/json'));
             $response->end();
             return;
+        case '/chat':
+            $id = $request->getHeaderLine('Last-Event-ID');
+
+            $response->writeHead(200, array('Content-Type' => 'text/event-stream'));
+            $channel->connect($response, $id);
+
+            $message = array('message' => 'New person connected from '. $request->remoteAddress);
+            $channel->writeMessage(json_encode($message));
+
+            $response->on('close', function () use ($response, $channel, $request) {
+                $channel->disconnect($response);
+
+                $message = array('message' => 'Bye '. $request->remoteAddress);
+                $channel->writeMessage(json_encode($message));
+            });
+            break;
+        default:
+            $response->writeHead(404);
+            $response->end('Not Found');
     }
-
-    echo $request->remoteAddress . ' connected' . PHP_EOL;
-
-    $headers = $request->getHeaders();
-    $id = isset($headers['Last-Event-ID']) ? $headers['Last-Event-ID'] : null;
-
-    $response->writeHead(200, array('Content-Type' => 'text/event-stream'));
-    $channel->connect($response, $id);
-
-    $response->on('close', function () use ($response, $channel, $request) {
-        echo $request->remoteAddress . 'disconnected' . PHP_EOL;
-        $channel->disconnect($response);
-    });
 });
 
 $socket->listen(isset($argv[1]) ? $argv[1] : 0, '0.0.0.0');
